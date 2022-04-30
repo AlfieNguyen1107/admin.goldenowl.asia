499f1d9a71804dea238012d3fbb4b3f3c47937ec
Justin Nguyen <justin.ntt@gmail.com>
2022-04-30 23:03:45 +0700

    build a dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,16 +4,8 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @pagy, @users = pagy(extract_user, items: per_page)
   end
 
   private
 
-  def extract_user
-    if params[:search]
-      User.search(params[:search])
-    else
-      User.all.order(id: :desc)
-    end
-  end
 end
--- app/javascript/application/index.js
+++ app/javascript/application/index.js
@@ -2,5 +2,4 @@ import "./hyper";
 import "./components";
 import "./pages";
 
-import "bootstrap-icons/font/bootstrap-icons.css";
 import "../stylesheets/application.scss";
