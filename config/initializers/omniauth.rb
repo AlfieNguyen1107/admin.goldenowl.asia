acae0134bcadd3b998983e3c1296a467f24ea784
zoe-nguyen-goldenowl <zoe.nguyen.goldenowl@gmail.com>
2022-05-16 10:56:44 +0700

    change ENV in omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '1085748057063-39qc0kmdjm98i89p4am4vc4kgvvkop5b.apps.googleusercontent.com', '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',
+    
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
   {
     scope: "email",
     prompt: 'select_account',
