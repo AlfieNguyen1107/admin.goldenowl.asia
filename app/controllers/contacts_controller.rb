640c6cf9cc3e11da2a7b1cd5df36d5613bcb1294
dinh son <son.dinh.goldenowl@gmail.com>
2022-05-18 11:05:07 +0700

    Update logic CRUD contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,12 @@
 class ContactsController < ApplicationController
   before_action :set_contact, only: %i[show edit update destroy]
+
   def index
-    @pagy, @contacts = pagy(extract_contact, items: per_page)
+    @contacts = Contact.all
+    @pagy, @contacts = pagy(@contacts.order(id: :asc), items: per_page)
   end
 
-  def show
-    @pagy, @projects = pagy(@contact.projects, items: per_page)
-  end
+  def show; end
 
   def new
     @contact = Contact.new
@@ -55,12 +55,6 @@ class ContactsController < ApplicationController
   end
 
   def contact_params
-    params.require(:contact).permit(:name, :address, :hq)
-  end
-
-  def extract_contact
-    return Contact.search(params[:search]) if params[:search]
-
-    Contact.order(id: :desc)
+    params.require(:contact).permit(:full_name, :primary_phone_number, :company_homepage_url, :company_linkedin_url, :personal_linkedin_url, :personal_facebook_url, :personal_twitter_url, :based_nationality, :current_living_country)
   end
 end
