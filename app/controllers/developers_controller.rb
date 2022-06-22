e00ef05fa80aa95455eb8ef44f3f20da08030dbd
zoe-nguyen-goldenowl <zoe.nguyen.goldenowl@gmail.com>
2022-06-22 17:43:33 +0700

    generate file pdf to profile developer

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -18,7 +18,10 @@ class DevelopersController < ApplicationController
     respond_to do |format|
       format.html
       format.pdf do
-        render pdf: 'file.pdf', template: 'developers/export.html.slim'
+        render pdf: 'file.pdf',
+               template: 'developers/export.html.slim',
+               layout: 'pdf',
+               page_size: 'A4'
       end
     end
   end
